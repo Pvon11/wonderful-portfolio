6fd68edd659ed317e1edea6174e04532ebfa063b
Pvon11 <patrickvonatbone@gmail.com>
2023-07-20 19:03:00 -0400

    color stuff

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -45,7 +45,7 @@ const ContactForm = () => {
       <button
         type="submit"
         disabled={state.submitting}
-        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-semibold"
+        className="bg-notindigo hover:bg-notslate text-white px-4 py-2 rounded-md font-semibold"
       >
         Submit
       </button>
